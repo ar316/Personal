a58de3a20e00e9f7b752b525ae1bfad6a0a03d1d
Arnold Briñez <45498184+ar316@users.noreply.github.com>
2023-05-07 23:07:49 -0500

    no impirme en columnas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const agregar = document.querySelector('.BtnAgregar');
 const form = document.querySelector('.form');
 const listaEventos = document.querySelector('.mostrarEventos');
+const listaEventos2 = document.querySelector('#listaEventos tbody');
 let cant = [];
 
 cargarEventos();
@@ -19,11 +20,43 @@ function AgregarEnvento(e){
     hora: form.querySelector('#hora').value,
    }
    cant = [...cant, infoEvento];
+   form.reset();
+   const tabla = document.querySelector('#listaEventos');
+
+   cant.forEach( e  => {
+    const row  = document.createElement('tr');
+    row.innerHTML=`
+        
+        <td> 
+            ${infoEvento.nombre}
+        </td>
+        <td> 
+            ${infoEvento.fecha}
+        </td>
+        <td> 
+        ${infoEvento.hora}
+        </td>`
+    
+        listaEventos2.appendChild(row);
+   })
+
 }
+
+
 //creando el objeto para guardar los eventos 
 function  mostrar(){
     cant.forEach( e => {
         console.table(e);
     })
 }
-mostrar();
+
+
+
+
+function  limpiarHtml() {
+    //forma lenta 
+    listaEventos2.innerHTML = '';
+
+    //forma rapida 
+    
+}
